b1579d626267143712bffc2b4b0e1dad1b05514a
Beppe94 <giuseppe.monorchio94@hotmail.it>
2025-02-04 19:35:28 +0100

    add express validator for game form and ejs to sh show errors, fix controller to pass data to form instead of re rendering page

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,30 +1,49 @@
 import { getGames, insertGameInDB } from "../Database/Queries.js";
+import { body, validationResult } from "express-validator";
+
+const validateGameFields = [
+    body("title").trim()
+    .notEmpty().withMessage("Field is empty"),
+    body("image").trim()
+    .notEmpty().withMessage("Field is empty")
+    .isURL().withMessage("Must be a valid URL")
+    .matches(/\.(jpeg|jpg|png)$/i).withMessage("URL must point to an image"),
+    body("price").trim()
+    .notEmpty().withMessage("Field is empty")
+    .isFloat({max: 100}).withMessage("Price must be within 100"),
+    body("reviews").trim()
+    .notEmpty().withMessage("Field is empty")
+    .isFloat({max: 10}).withMessage("Review must be within 10"),
+    body("genre").trim()
+    .notEmpty().withMessage("Field is empty")
+]
 
 export async function getHomepage(req, res) {
     const data = await getGames();
 
-    console.log(data);
-    res.render("home", {data: data});
+    //console.log(data);
+    res.status(200).render("home", {data: data});
 }
 
 export function newGameForm(req, res) {
-    res.render("newGame");
+    res.status(200).render("newGame", {data: {} , errors: []});
 }
 
-export async function newGamePost(req, res) {
-    const {
-        title, 
-        description,
-        image,
-        price,
-        publisher,
-        reviews,
-        genre
-    } = req.body;
-
-    console.log(title,description, image ,price,publisher,reviews,genre);
-
-    await insertGameInDB({title, description, image, price, publisher, reviews, genre})
-
-    res.redirect("/")
-}
+export const newGamePost = [validateGameFields, async (req, res) => {
+    const errors = validationResult(req);
+    const {title, image, price,
+        reviews, genre} = req.body;
+        
+    console.log(errors.array());
+    
+    
+    if(!errors.isEmpty()) {
+        return res.status(400).render("newGame", {
+            data: {title, image, price, reviews, genre},
+            errors: errors.array()
+        })
+    }
+    
+    await insertGameInDB({title, image, price, reviews, genre})
+    res.redirect("/");
+}]
--- Database/Queries.js
+++ Database/Queries.js
@@ -8,17 +8,15 @@ export async function getGames() {
 
 export async function insertGameInDB(data) {
     const {
-        title, 
-        description,
+        title,
         image,
         price,
-        publisher,
         reviews,
         genre
     } = data;
 
-    const game = await client.query(`INSERT INTO games (title, description, image, price) VALUES ($1, $2, $3, $4) RETURNING id`, [title, description, image, price])
+    const game = await client.query(`INSERT INTO games (title, image, price) VALUES ($1, $2, $3) RETURNING id`, [title, image, price])
     const gameId = game.rows[0].id
 
-    await client.query(`INSERT INTO info (game_id, publisher, reviews, genre) VALUES ($1, $2, $3, $4)`, [gameId, publisher, reviews, genre])
+    await client.query(`INSERT INTO info (game_id, reviews, genre) VALUES ($1, $2, $3)`, [gameId, reviews, genre])
 }
