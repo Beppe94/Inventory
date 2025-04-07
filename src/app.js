476aa996f534bc7ce22a1dccc61acab7b45b400d
Beppe94 <giuseppe.monorchio94@hotmail.it>
2025-04-07 15:52:39 +0200

    fix deploy test

--- src/app.js
+++ src/app.js
@@ -2,15 +2,31 @@ import express from "express";
 import route from "../routes/router.js";
 import "dotenv/config";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const ENV = process.env;
 
-app.set("views", path.join(process.cwd(), "views"));
+const viewsPath = process.env.NETLIFY 
+    ? path.join(process.cwd(), 'views')
+    : process.env.NETLIFY_DEV
+    ? path.join(__dirname, '../views')
+    : path.join(__dirname, 'views');
+
+app.set("views", viewsPath);
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join("./public")));
+
+const staticPath = process.env.NETLIFY
+  ? path.join(process.cwd(), 'public')
+  : path.join(__dirname, '../public');
+app.use(express.static(staticPath));
+
+app.use(express.static(staticPath));
 app.use("/", route);
 
 //app.listen(ENV.PORT, () => console.log(`Listening on port: ${ENV.PORT}`));
