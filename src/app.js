3c320b5d0459283ee2c3252787ac97358052318d
Beppe94 <giuseppe.monorchio94@hotmail.it>
2025-04-07 16:35:59 +0200

    fix deployment should work now

--- src/app.js
+++ src/app.js
@@ -2,28 +2,33 @@ import express from "express";
 import route from "../routes/router.js";
 import "dotenv/config";
 import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __file = fileURLToPath(import.meta.url);
-const __dir = path.dirname(__file);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const ENV = process.env;
 
 const viewsPath = process.env.NETLIFY 
-    ? path.join(process.cwd(), 'views')
-    : process.env.NETLIFY_DEV
-    ? path.join(__dir, '../views')
-    : path.join(__dir, 'views');
+    ? path.join(process.cwd(), 'views') 
+    : process.env.NETLIFY_DEV 
+    ? path.join(__dirname, '../views')
+    : path.join(__dirname, 'views');
 
 app.set("views", viewsPath);
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const staticPath = process.env.NETLIFY
-  ? path.join(process.cwd(), 'public')
-  : path.join(__dir, '../public');
+const staticPath = process.env.NETLIFY 
+    ? path.join(process.cwd(), 'public')
+    : path.join(__dirname, '../public');
+
+console.log(staticPath);
+
+
 app.use(express.static(staticPath));
 
 app.use(express.static(staticPath));
